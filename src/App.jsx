761dbb2f123fb7bb6a73a590a76a66249e72f107
abrahamuchos <j.abraham29@gmail.com>
2024-11-01 18:08:21 -0400

    Add styles config

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
+      <h1 className="text-3xl font-Stora font-light">
         Hello world!
       </h1>
+      <p className='text-lg font-GameOverCre'>Hello Abraham</p>
+      <p className='text-lg font-YoungSerif'>Hello Abraham</p>
+      <p>Hola abraham</p>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,31 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontSize:{
+      xs: ['12px', '24px'],
+      sm: ['14px', '24px'],
+      base: ['16px', '24px'],
+      baseDeco: ['16px', '60.91px'],
+      lg: ['20px', '32px'],
+      xl: ['24px', '48px'],
+      xlDeco: ['24px', '60.91px'],
+      '2xl': ['32px', '52px'],
+      '3xl': ['50px', '60.91px'],
+    },
+    extend: {
+      fontFamily:{
+        'Stora': ['Stora', 'sans-serif'],
+        'GameOverCre': ['gameovercre1', 'sans-serif'],
+        'YoungSerif': ['YoungSerif-Regular', 'sans-serif'],
+      },
+      colors:{
+        'blueOlga': '#006AA8',
+        'redOlga': '#FF0044',
+        'blackOlga': '#1B1B1B',
+        'yellowOlga': '#FDCD09',
+        'creamOlga': '#FCF4E0',
+      }
+    },
   },
   plugins: [],
 }
